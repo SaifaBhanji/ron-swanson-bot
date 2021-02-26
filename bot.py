06d763a74ec7e75bd3eaf3cdbfe0be0716761716
Saifa Bhanji <bhanji.saifa@gmail.com>
2021-02-25 22:40:26 -0500

    added !boop command

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 DICTIONARY_API_ID = os.getenv('DICTIONARY_API_ID')
 DICTIONARY_API_KEY = os.getenv('DICTIONARY_API_KEY')
+RURU_ID = os.getenv('RURU_ID')
+SAIFA_ID = os.getenv('SAIFA_ID')
 
 bot = commands.Bot(command_prefix='!')
 
@@ -25,11 +27,11 @@ async def ron_swanson(ctx):
     response = requests.get(
         "http://ron-swanson-quotes.herokuapp.com/v2/quotes")
     json_data = json.dumps(response.json())
-    quote = json_data.replace('[', '').replace(']', '').replace('"', '')
+    quote = json_data[2:-2]
     await ctx.send(quote)
 
 
-@bot.command(name='define', help='You turned me into a dictionary??', pass_context=True)
+@bot.command(name='define', help='You turned me into a dictionary?!', pass_context=True)
 async def ron_swanson(ctx, *, word):
 
     if len(word.split()) > 1:
@@ -44,5 +46,23 @@ async def ron_swanson(ctx, *, word):
         definition = response["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
 
         await ctx.send(word + " : " + definition)
+
+
+@bot.command(name='boop', help='You guys are gross.', pass_context=True)
+async def ron_swanson(ctx, *, person):
+
+    if len(person.split()) > 1:
+        await ctx.send("Hold on! I can only boop one person at a time!")
+
+    if (person.lower() == 'ruru'):
+        user = await bot.fetch_user(RURU_ID)
+        await user.send("Saifa is booping you!")
+        await ctx.send("I booped her.")
+    elif (person.lower() == 'saifa'):
+        user = await bot.fetch_user(SAIFA_ID)
+        await user.send("Ruru is booping you!")
+        await ctx.send("I booped him.")
+    else:
+        await ctx.send("Sorry, I can't boop that person.")
 
 bot.run(TOKEN)
